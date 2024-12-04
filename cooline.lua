314c69f0324f0a9519f78992bc6eb7f2579987da
Ironscyde <141408324+Ironscyde@users.noreply.github.com>
2024-12-04 08:14:16 +0000

    Update cooline.lua

--- cooline.lua
+++ cooline.lua
@@ -4,7 +4,7 @@ cooline:SetScript('OnEvent', function()
 end)
 cooline:RegisterEvent('VARIABLES_LOADED')
 
-cooline_settings = { x = 0, y = -240 }
+cooline_settings = { x = 0, y = -250 }
 
 local frame_pool = {}
 local cooldowns = {}
